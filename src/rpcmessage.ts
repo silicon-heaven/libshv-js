7e5967ba2afe992f62eb6808526da4284e659565
Václav Kubernát <sir.venceslas@gmail.com>
2024-07-31 15:37:33 +0200

    Make classes for separate RpcErrors

--- src/rpcmessage.ts
+++ src/rpcmessage.ts
@@ -11,6 +11,54 @@ const KeyParams = 1;
 const KeyResult = 2;
 const KeyError = 3;
 
+export const ERROR_CODE = 1;
+export const ERROR_MESSAGE = 2;
+export const ERROR_DATA = 3;
+
+enum ErrorCode {
+    InvalidRequest = 1,
+    MethodNotFound = 2,
+    InvalidParams = 3,
+    InternalError = 4,
+    ParseError = 5,
+    MethodCallTimeout = 6,
+    MethodCallCancelled = 7,
+    MethodCallException = 8,
+    Unknown = 9,
+    LoginRequired = 10,
+    UserIDRequired = 11,
+    NotImplemented = 12,
+}
+
+type ErrorMap = IMap<{
+    [ERROR_CODE]: Int<ErrorCode>;
+    [ERROR_MESSAGE]?: string;
+    [ERROR_DATA]?: RpcValue;
+}>;
+
+class RpcError extends Error {
+    constructor(private readonly err_info: ErrorMap) {
+        super(err_info.value[ERROR_MESSAGE]);
+    }
+
+    data() {
+        return this.err_info.value[ERROR_DATA];
+    }
+}
+
+export class InvalidRequest extends RpcError {}
+export class MethodNotFound extends RpcError {}
+export class InvalidParams extends RpcError {}
+export class InternalError extends RpcError {}
+export class ParseError extends RpcError {}
+export class MethodCallTimeout extends RpcError {}
+export class MethodCallCancelled extends RpcError {}
+export class MethodCallException extends RpcError {}
+export class Unknown extends RpcError {}
+export class LoginRequired extends RpcError {}
+export class UserIDRequired extends RpcError {}
+export class NotImplemented extends RpcError {}
+
 class RpcMessage {
     value: IMap;
     meta: MetaMap;
@@ -87,7 +135,24 @@ class RpcMessage {
 
     resultOrError() {
         if (this.value.value[KeyError] !== undefined) {
-            return new RpcError(this.value.value[KeyError] as ErrorMap);
+            const ErrorType = (() => {
+                switch ((this.value.value[KeyError] as ErrorMap).value[ERROR_CODE].value) {
+                    case ErrorCode.InvalidRequest: return InvalidRequest;
+                    case ErrorCode.MethodNotFound: return MethodNotFound;
+                    case ErrorCode.InvalidParams: return InvalidParams;
+                    case ErrorCode.InternalError: return InternalError;
+                    case ErrorCode.ParseError: return ParseError;
+                    case ErrorCode.MethodCallTimeout: return MethodCallTimeout;
+                    case ErrorCode.MethodCallCancelled: return MethodCallCancelled;
+                    case ErrorCode.MethodCallException: return MethodCallException;
+                    case ErrorCode.Unknown: return Unknown;
+                    case ErrorCode.LoginRequired: return LoginRequired;
+                    case ErrorCode.UserIDRequired: return UserIDRequired;
+                    case ErrorCode.NotImplemented: return NotImplemented;
+                }
+            })();
+
+            return new ErrorType(this.value.value[KeyError] as ErrorMap);
         }
 
         return this.value.value[KeyResult];
@@ -110,46 +175,6 @@ class RpcMessage {
     }
 }
 
-export const ERROR_CODE = 1;
-export const ERROR_MESSAGE = 2;
-export const ERROR_DATA = 3;
-
-enum ErrorCode {
-    InvalidRequest = 1,
-    MethodNotFound = 2,
-    InvalidParams = 3,
-    InternalError = 4,
-    ParseError = 5,
-    MethodCallTimeout = 6,
-    MethodCallCancelled = 7,
-    MethodCallException = 8,
-    Unknown = 9,
-    LoginRequired = 10,
-    UserIDRequired = 11,
-    NotImplemented = 12,
-}
-
-type ErrorMap = IMap<{
-    [ERROR_CODE]: Int<ErrorCode>;
-    [ERROR_MESSAGE]?: string;
-    [ERROR_DATA]?: RpcValue;
-}>;
-
-class RpcError {
-    constructor(private readonly err_info: ErrorMap) {}
-    code() {
-        return this.err_info.value[ERROR_CODE];
-    }
-
-    message() {
-        return this.err_info.value[ERROR_MESSAGE];
-    }
-
-    data() {
-        return this.err_info.value[ERROR_DATA];
-    }
-}
-
-export type RpcResponse<T = RpcValue> = T | RpcError;
+export type RpcResponse<T = RpcValue> = T | Error;
 
 export {RpcMessage, RpcError, ErrorCode};
