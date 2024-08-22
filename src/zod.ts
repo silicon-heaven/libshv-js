168ea2b02ed08ccb6b1889e2aac9840034c00c15
Jaroslav Beran <jara.beran@gmail.com>
2024-08-22 11:52:45 +0200

    zod: add validator for ShvMap of Record<string, T>
    Validator for ShvMap where keys are any strings and values
    have a specific structure.

--- src/zod.ts
+++ src/zod.ts
@@ -2,6 +2,7 @@ import {z, type ZodType} from 'zod';
 import {IMap, Int, type RpcValue, ShvMap} from './rpcvalue';
 
 export const map = <T extends Record<string, ZodType<any, any, any>>>(schema: T) => z.custom<ShvMap<{[K in keyof T]: z.infer<T[K]>}>>((data: RpcValue) => data instanceof ShvMap && z.object(schema).safeParse(data.value).success);
+export const recmap = <T extends ZodType<any, any, any>>(schema: T) => z.custom<ShvMap<Record<string, z.infer<T>>>>((data: RpcValue) => data instanceof ShvMap && z.record(z.string(), schema).safeParse(data.value).success);
 export const imap = <T extends Record<string, ZodType<any, any, any>>>(schema: T) => z.custom<IMap<{[K in keyof T]: z.infer<T[K]>}>>((data: RpcValue) => data instanceof IMap && z.object(schema).safeParse(data.value).success);
 export const int = () => z.custom<Int>((data: RpcValue) => data instanceof Int);
 
