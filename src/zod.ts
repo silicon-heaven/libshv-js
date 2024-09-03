49d8ebe93e32c613f7e85e6d3203ba4cccac4e76
Václav Kubernát <sir.venceslas@gmail.com>
2024-09-03 15:25:06 +0200

    zod: Add UInt

--- src/zod.ts
+++ src/zod.ts
@@ -1,9 +1,10 @@
 import {z, type ZodType} from 'zod';
-import {type IMap, Int, isIMap, isShvMap, type RpcValue, type ShvMap} from './rpcvalue';
+import {type IMap, Int, isIMap, isShvMap, type RpcValue, type ShvMap, UInt} from './rpcvalue';
 
 export const map = <T extends Record<string, ZodType<any, any, any>>>(schema: T) => z.custom<ShvMap<{[K in keyof T]: z.infer<T[K]>}>>((data: RpcValue) => isShvMap(data) && z.object(schema).safeParse(data).success);
 export const recmap = <T extends ZodType<any, any, any>>(schema: T) => z.custom<ShvMap<Record<string, z.infer<T>>>>((data: RpcValue) => isShvMap(data) && z.record(z.string(), schema).safeParse(data).success);
 export const imap = <T extends Record<string, ZodType<any, any, any>>>(schema: T) => z.custom<IMap<{[K in keyof T]: z.infer<T[K]>}>>((data: RpcValue) => isIMap(data) && z.object(schema).safeParse(data).success);
 export const int = () => z.custom<Int>((data: RpcValue) => data instanceof Int);
+export const uint = () => z.custom<UInt>((data: RpcValue) => data instanceof UInt);
 
 export * from 'zod';
