d5bcee2bf3ad6444c557d80f720cad653dc2f8ab
Václav Kubernát <sir.venceslas@gmail.com>
2024-09-05 11:12:59 +0200

    Add generic argument to UInt

--- src/zod.ts
+++ src/zod.ts
@@ -6,6 +6,7 @@ export const recmap = <T extends ZodType<any, any, any>>(schema: T) => z.record(
 export const imap = <T extends Record<string, ZodType<any, any, any>>>(schema: T) => z.object(schema).and(z.object({[shvMapType]: z.literal('imap')}));
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-arguments -- Zod needs the default argument, otherwise it'll infer as Int<unknown>
 export const int = () => z.instanceof(Int<number>);
-export const uint = () => z.instanceof(UInt);
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-arguments -- Zod needs the default argument, otherwise it'll infer as UInt<unknown>
+export const uint = () => z.instanceof(UInt<number>);
 
 export * from 'zod';
